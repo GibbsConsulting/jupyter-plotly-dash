0f7ec73cf6b150611c20ee7bde839032944731e8
Dimitar Vasilev <d.vasilev@gmail.com>
2021-01-07 04:34:41 +0200

    Update argument name expected by the dash package (#60)
    Co-authored-by: Vasilev, Dimitar <Dimitar.Vasilev@experian.com>

--- jupyter_plotly_dash/dash_wrapper.py
+++ jupyter_plotly_dash/dash_wrapper.py
@@ -211,13 +211,13 @@ class JupyterDash:
             dapp.index()
             # Endpoint is dash-component-suites/package_name/path_in_package
             try:
-                mFunc = dapp.locate_endpoint_function('dash-component-suites/<string:package_name>/<path:path_in_package_dist>')
+                mFunc = dapp.locate_endpoint_function('dash-component-suites/<string:package_name>/<path:fingerprinted_path>')
             except Exception as e:
                 return ("<html><body>Requested %s at %s with %s and failed with %s</body></html>" %(args,app_path,view_name_parts,e),"text/html")
             # Need two arguments here: package_name and path_in_package_dist
             package_name = view_name_parts[1]
             path_in_package_dist = "/".join(view_name_parts[2:])
             resp = mFunc(package_name=package_name,
-                         path_in_package_dist=path_in_package_dist)
+                         fingerprinted_path=path_in_package_dist)
             return (resp.data.decode('utf-8'), resp.mimetype)
 
